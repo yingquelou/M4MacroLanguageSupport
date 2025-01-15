12d2896eeea3c232107434109d7f463e2c687e45
yingquelou <1720042096@qq.com>
2025-01-15 22:41:40 +0800

    redefine

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import { m4RegisterCommand, m4RegisterCompletionItemProvider, m4GetTerminal, m4RegisterDocumentSymbolProvider } from './m4init';
+import { m4RegisterCommand, m4RegisterCompletionItemProvider, m4RegisterDocumentSymbolProvider } from './m4init';
 import * as fs from 'fs';
 
 // This method is called when your extension is activated
--- src/m4init.ts
+++ src/m4init.ts
@@ -1,7 +1,5 @@
 import * as vscode from 'vscode';
-import * as path from 'path';
-import * as fs from 'fs';
-import { m4fparser, m4parser } from './m4fparser';
+import { exec } from 'child_process';
 function m4RegisterCommand(context: vscode.ExtensionContext) {
     context.subscriptions.push(vscode.commands.registerCommand('test.helloWorld', () => {
         // The code you place here will be executed every time your command is executed
@@ -10,44 +8,6 @@ function m4RegisterCommand(context: vscode.ExtensionContext) {
     }));
 }
 
-var m4ter: vscode.Terminal | undefined;
-var configfile: string = "";
-var defaultWindowsProfile: string = "terminal.integrated.defaultProfile.windows";
-function m4GetTerminal(context: vscode.ExtensionContext) {
-    if (m4ter !== undefined && m4ter.exitStatus === undefined) { return m4ter; }
-    if (context.storageUri) {
-        configfile = path.join(context.globalStorageUri.fsPath, 'm4.conf.json');
-        if (fs.existsSync(configfile)) {
-            var def = JSON.parse(fs.readFileSync(configfile).toString())[defaultWindowsProfile];
-
-            vscode.workspace.getConfiguration().update(defaultWindowsProfile, def, vscode.ConfigurationTarget.Workspace).then(
-                _ => {
-                    m4ter = m4ter ? m4ter : vscode.window.createTerminal();
-                }
-            );
-        } else {
-            const opt: string[] = [];
-            var ter = vscode.workspace.getConfiguration("terminal.integrated.profiles.windows");
-            for (var i in JSON.parse(JSON.stringify(ter))) {
-                opt.push(i);
-            }
-            vscode.window.showQuickPick(opt, { title: "选择提供m4的终端环境" }).then((v) => {
-                if (v) {
-                    // var config: any = ter.get(v)
-                    fs.mkdirSync(context.globalStorageUri.fsPath);
-                    fs.writeFileSync(configfile, JSON.stringify({ [defaultWindowsProfile]: v }));
-                    vscode.workspace.getConfiguration().update(defaultWindowsProfile, v, vscode.ConfigurationTarget.Workspace
-                    ).then(
-                        (v) => {
-                            m4ter = m4ter ? m4ter : vscode.window.createTerminal();
-                        }
-                    );
-                }
-            });
-        }
-    }
-    return m4ter;
-}
 function m4RegisterCompletionItemProvider(context: vscode.ExtensionContext) {
     // 创建多个补全项
     const completionItems: vscode.CompletionItem[] = [];
@@ -67,38 +27,34 @@ function m4RegisterCompletionItemProvider(context: vscode.ExtensionContext) {
     );
 }
 function m4RegisterDocumentSymbolProvider(context: vscode.ExtensionContext) {
-    var ter = m4GetTerminal(context);
-    vscode.workspace.fs.writeFile;
-    vscode.workspace.fs.createDirectory(context.globalStorageUri)
-    var buildin = vscode.Uri.joinPath(context.globalStorageUri, fs.mkdtempSync('m4')+".m4");
+    const m4config = vscode.workspace.getConfiguration("m4");
+    if (!m4config.has("path")) {
+        vscode.window.showWarningMessage("m4 path undefine in settings.json")
+        return;
+    }
+    var path = m4config.get<string>("path");
+    if (path) {
+        const reg = /\${env:(.*)}/
+        const match = path.match(reg)
+        if (match) {
+            path = path.replace(match[0], process.env[match[1]] as string).replace("\\", "/")
+            // vscode.workspace.openTextDocument({ content: path })
+        }
+        exec([path, "--version"].join(" "), (error, out, err) => {
+            if (error) { vscode.window.showWarningMessage(error.message); return; }
+            if (err) { vscode.window.showWarningMessage(err); return; }
+            // if (out) { vscode.window.showInformationMessage(out) }
+            exec([path].join(" "))
+            context.subscriptions.push(
+                vscode.languages.registerDocumentSymbolProvider('m4', {
+                    async provideDocumentSymbols(document, token) {
+                        const dss: vscode.DocumentSymbol[] = [];
 
-    var bds: vscode.DocumentSymbol | undefined;
-    context.subscriptions.push(
-        vscode.languages.registerDocumentSymbolProvider('m4', {
-            async provideDocumentSymbols(document, token) {
-                const dss = [];
-                if (ter === undefined) {
-                    ter = m4GetTerminal(context);
-                }
-                if (!bds) {
-                    await vscode.workspace.fs.writeFile(buildin, new Uint8Array());
-                    var bp = buildin.path.split(/:/).join('').replace(/\s/,"\\ ");
-                    ter?.sendText([
-                        "m4", `"${bp}"`, "-F", `${bp}f`
-                    ].join(" "), true);
-                   var bf= vscode.Uri.file(buildin.path.concat('f'))
-                    var bdoc = await vscode.workspace.openTextDocument(bf);
-                    bds = m4fparser(bdoc);
-                }
-    
-                var f = document.uri.path.split(/:/).join('').replace(/\s/,"\\ ");
-                ter?.sendText([
-                    "m4", f, "-F", `${f}f`
-                ].join(" "), true);
-                var doc = await vscode.workspace.openTextDocument(vscode.Uri.file(document.uri.path.concat('f')));
-                dss.push(...m4parser(bds, document, doc));
-                return dss;
-            },
-        }));
+
+                        return dss;
+                    },
+                }));
+        })
+    }
 }
-export { m4RegisterDocumentSymbolProvider, m4RegisterCommand, m4GetTerminal, m4RegisterCompletionItemProvider };
+export { m4RegisterDocumentSymbolProvider, m4RegisterCommand, m4RegisterCompletionItemProvider };
